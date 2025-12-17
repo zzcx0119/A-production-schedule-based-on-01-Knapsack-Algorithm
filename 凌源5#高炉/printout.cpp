be9f5ec8412afa4173bda6fc04c66db28f87d433
zzcx <1793835070@qq.com>
2025-12-18 00:42:37 +0800

    Add files via upload
    少打个数据

---
+++ 凌源5#高炉/printout.cpp
@@ -0,0 +1,11 @@
+#include"paichan.h"
+
+void paichan::printout(int n, ofstream& outFile)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        outFile << bricks[i].chosen_time << " ";
+    }
+    outFile << endl;
+}
---
+++ 凌源5#高炉/process.cpp
@@ -0,0 +1,81 @@
+#include"paichan.h"
+
+int First_batch = 12;
+int First_batch_day = 2;
+int Second_batch = 15;
+int Second_batch_day = 4;
+int Third_batch = 29;
+int Third_batch_day = 6;
+int Fourth_batch = 39;
+int Fourth_batch_day = 8;
+int Fifth_batch = 46;
+int Fifth_batch_day = 29;
+int MAX_WEIGHT = 2000;
+int maxnum1 = 1;
+int maxnum2 = 2;
+
+void paichan::process(ofstream& outFile, ofstream& outFile1)
+{
+
+	for (int day = 1; day <= First_batch_day; day++)
+	{
+		clear(First_batch);
+		outFile1 << "第" << day << "天" << endl;
+		outFile1 << "开始进行编排..." << endl;
+		outFile1 << endl;
+		getprice(First_batch);
+		backpack(First_batch, MAX_WEIGHT, maxnum1, outFile1);
+		outFile1 << endl;
+		printout(First_batch, outFile);
+	}
+	for (int day = First_batch_day + 1; day <= Second_batch_day; day++)
+	{
+		clear(Second_batch);
+		outFile1 << "第" << day << "天" << endl;
+		outFile1 << "开始进行编排..." << endl;
+		outFile1 << endl;
+		getprice(Second_batch);
+		backpack(Second_batch, MAX_WEIGHT, maxnum1, outFile1);
+		outFile1 << endl;
+		printout(Second_batch, outFile);
+	}
+
+	for (int day = Second_batch_day + 1; day <= Third_batch_day; day++)
+	{
+		clear(Third_batch);
+		outFile1 << "第" << day << "天" << endl;
+		outFile1 << "开始进行编排..." << endl;
+		outFile1 << endl;
+		getprice(Third_batch);
+		backpack(Third_batch, MAX_WEIGHT, maxnum1, outFile1);
+		outFile1 << endl;
+		printout(Third_batch, outFile);
+	}
+
+	for (int day = Third_batch_day + 1; day <= Fourth_batch_day; day++)
+	{
+		clear(Fourth_batch);
+		outFile1 << "第" << day << "天" << endl;
+		outFile1 << "开始进行编排..." << endl;
+		outFile1 << endl;
+		getprice(Fourth_batch);
+		backpack(Fourth_batch, MAX_WEIGHT, maxnum1, outFile1);
+		outFile1 << endl;
+		printout(Fourth_batch, outFile);
+	}
+
+	//changenum(Fifth_batch);
+	for (int day = Fourth_batch_day + 1; day <= Fifth_batch_day; day++)
+	{
+		clear(Fifth_batch);
+		outFile1 << "第" << day << "天" << endl;
+		outFile1 << "开始进行编排..." << endl;
+		outFile1 << endl;
+		getprice(Fifth_batch);
+		backpack(Fifth_batch, MAX_WEIGHT, maxnum2, outFile1);
+		outFile1 << endl;
+		printout(Fifth_batch, outFile);
+	}
+	cout << "编排完成！" << endl;
+	cout << endl;
+}
