cbe5913144efd321e83a61b332422f9e233c013d
zzcx <1793835070@qq.com>
2025-12-17 20:15:44 +0800

    Add files via upload

---
+++ 排产/paichan.h
@@ -0,0 +1,34 @@
+#pragma once
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<fstream>
+using namespace std;
+
+
+struct product
+{
+	string type;
+	int left_number = 0;
+	int weight = 0;
+	int price = 0;
+	int chosen_time = 0;
+	//int everytime_num_max = 1;
+	bool flag = 1;//Õë¶ÔR,L½áÎ²µÄ×©
+};
+
+class paichan
+{
+public:
+	void getprice(int);
+	void changenum(int);
+	void initialize();
+	void clear(int);
+	void backpack(int, int, int, ofstream&);
+	void process(ofstream&, ofstream&);
+	void printout(int,ofstream&);
+	//bool is_batch_empty();
+private:
+	product bricks[45];
+};
